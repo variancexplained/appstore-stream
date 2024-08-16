603328f78fb68b1ff0c4499dac0df216d4d6666a
John James <john@variancexplained.com>
2024-08-16 11:59:50 -0400

    task metrics

---
+++ appstorestream/application/metrics/task..py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : AppStoreStream: Apple App Data and Reviews, Delivered!                              #
+# Version    : 0.1.0                                                                               #
+# Python     : 3.10.14                                                                             #
+# Filename   : /appstorestream/application/metrics/task..py                                        #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john@variancexplained.com                                                           #
+# URL        : https://github.com/variancexplained/appstore-stream.git                             #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Thursday August 15th 2024 04:31:15 pm                                               #
+# Modified   : Friday August 16th 2024 11:59:42 am                                                 #
+# ------------------------------------------------------------------------------------------------ #
+# License    : MIT License                                                                         #
+# Copyright  : (c) 2024 John James                                                                 #
+# ================================================================================================ #
+"""Asynchronous Response Metrics Module"""
+import sys
+import time
+from dataclasses import dataclass, field
+
+import aiohttp
+import numpy as np
+from prometheus_client import Counter, Gauge
+
+from appstorestream.core.metrics import Metrics, MetricServer
+
+
+# ------------------------------------------------------------------------------------------------ #
+@dataclass
+class TaskMetrics(Metrics):
+    runtime_start_timestamp_seconds: float = None
+    runtime_stop_timestamp_seconds: float = None
+    runtime_duration_seconds: int = None
+
+    record_count_total: int = None
+    record_per_second_ratio: float = None
+
+    def start(self) -> None:
+        self.runtime_start_timestamp_seconds = time.time()
+
+    def stop(self) -> None:
+        self.runtime_stop_timestamp_seconds = time.time()
+        self.runtime_duration_seconds = (
+            self.runtime_stop_timestamp_seconds - self.runtime_start_timestamp_seconds
+        )
+        self._compute_record_metrics()
+
+    def add_record(self, record: dict) -> None:
+        self.record_count_total += 1
+
+    def _compute_record_metrics(self) -> None:
+        # Records per second
+        self.record_per_second_ratio = (
+            (self.record_count_total / self.runtime_duration_seconds)
+            if self.runtime_duration_seconds > 0
+            else 0
+        )
+
+
+# ------------------------------------------------------------------------------------------------ #
+class TaskMetricServer(MetricServer):
+    """Task Metric Server Class"""
+
+    def __init__(self, job_id: str, dataset: str, port: int = 8000):
+        super().__init__(job_id, dataset, port)
+        self.define_metrics()
+
+    def define_metrics(self):
+        """Defines the metrics for the task process."""
+
+        # Runtime metrics
+        self.task_runtime_start_timestamp_seconds = Counter(
+            "appstorestream_task_runtime_start_timestamp_seconds",
+            "Start timestamp in seconds",
+            self._labels.keys(),
+        )
+        self.task_runtime_stop_timestamp_seconds = Counter(
+            "appstorestream_task_runtime_stop_timestamp_seconds",
+            "Stop timestamp in seconds",
+            self._labels.keys(),
+        )
+        self.task_runtime_duration_seconds = Gauge(
+            "appstorestream_task_runtime_duration_seconds",
+            "Runtime in seconds",
+            self._labels.keys(),
+        )
+        self.task_runtime_duration_seconds_total = Counter(
+            "appstorestream_task_runtime_duration_seconds_total",
+            "Total runtime in seconds",
+            self._labels.keys(),
+        )
+
+        # Record metrics
+        self.task_record_count_total = Counter(
+            "appstorestream_task_record_count_total",
+            "Total number of records.",
+            self._labels.keys(),
+        )
+        self.task_record_per_second_ratio = Counter(
+            "appstorestream_task_record_per_second_ratio",
+            "Records processed per second",
+            self._labels.keys(),
+        )
+
+    def update_metrics(self, metrics):
+        """Update the metrics with the provided values."""
+
+        metrics = metrics.as_dict()
+
+        # Update the metrics with the provided values
+        self.task_runtime_start_timestamp_seconds.labels(**self._labels).set(
+            metrics.get("runtime_start_timestamp_seconds", 0)
+        )
+        self.task_runtime_stop_timestamp_seconds.labels(**self._labels).set(
+            metrics.get("runtime_stop_timestamp_seconds", 0)
+        )
+        self.task_runtime_duration_seconds.labels(**self._labels).set(
+            metrics.get("runtime_duration_seconds", 0)
+        )
+        self.task_runtime_duration_seconds_total.labels(**self._labels).inc(
+            metrics.get("runtime_duration_seconds", 0)
+        )
+
+        self.task_record_count_total.labels(**self._labels).inc(
+            metrics.get("record_count_total", 0)
+        )
+        self.task_record_per_second_ratio.labels(**self._labels).inc(
+            metrics.get("record_per_second_ratio", 0)
+        )
